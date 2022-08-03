4982e33ba1335a1f2f153a5c343308b427313814
Matt <97047080+mattxmade@users.noreply.github.com>
2022-08-03 18:49:28 +0100

    Add react root element; import App Class component

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+import App from "./components/App";
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
