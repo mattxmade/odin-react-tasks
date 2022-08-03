787d6dab860db1598284bc33af849a9c3250634f
Matt <97047080+mattxmade@users.noreply.github.com>
2022-08-03 18:52:53 +0100

    Implement remove callback; handle id; render tasks

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,13 +1,36 @@
 import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Tasks = styled.ul`
+  list-style: none;
+`;
 
 const Overview = (props) => {
+  const tasks = props.tasks;
   return (
-    <ul>
-      {props.tasks.map((task, index) => {
-        <li key={index}>{task}</li>;
-      })}
-    </ul>
+    <Tasks className="tasks">
+      {tasks.map((task, index) => (
+        <li key={index} id={task.id}>
+          <p>
+            <span>{tasks.length === 0 ? "" : index + 1 + ". "}</span>
+            <span className="task">{task.value}</span>
+          </p>
+          <div className="icons">
+            <i className="fas fa-edit"></i>
+            <i
+              className="fas fa-trash-alt"
+              onClick={(e) => task.remove(e, task.id)}
+            ></i>
+          </div>
+        </li>
+      ))}
+    </Tasks>
   );
 };
 
 export default Overview;
+
+Overview.protoTypes = {
+  tasks: PropTypes.array,
+};
