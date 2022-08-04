6edf1433c7f67a04ebf4d0f2a93215beaed8618e
Matt <97047080+mattxmade@users.noreply.github.com>
2022-08-04 22:06:42 +0100

    Add input to Overview component; handle input; lift input value to parent App

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
@@ -8,19 +8,61 @@ const Tasks = styled.ul`
 
 const Overview = (props) => {
   const tasks = props.tasks;
+  const [value, setValue] = useState("");
+
+  const handleInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  const editTask = (e, task) => {
+    const edit = toggleIcon(task.id, e);
+    if (edit) task.action.update(task.id, value);
+  };
+
+  const toggleIcon = (taskID, e) => {
+    const itemInput = document.querySelector(`[data-id=${taskID}]`);
+    itemInput.classList.toggle("hide");
+
+    if (e.target.classList.contains("fa-user-edit")) {
+      e.target.classList.toggle("fa-user-edit");
+      e.target.classList.toggle("fa-user-check");
+
+      return false;
+    }
+
+    e.target.classList.toggle("fa-user-edit");
+    e.target.classList.toggle("fa-user-check");
+
+    return true;
+  };
+
   return (
     <Tasks className="tasks">
       {tasks.map((task, index) => (
         <li key={index} id={task.id}>
-          <p>
-            <span>{tasks.length === 0 ? "" : index + 1 + ". "}</span>
-            <span className="task">{task.value}</span>
-          </p>
-          <div className="icons">
-            <i className="fas fa-edit"></i>
+          <div className="task-container">
+            <p>{tasks.length === 0 ? "" : index + 1 + ". "}</p>
+
+            <div className="task-info">
+              <p className="task">{task.value}</p>
+              <input
+                type="text"
+                data-id={task.id}
+                placeholder={task.value}
+                onChange={handleInput}
+                className={"task-input hide"}
+              />
+            </div>
+          </div>
+
+          <div className="task-icons">
+            <i
+              className={"fas fa-user-edit"}
+              onClick={(e) => editTask(e, task)}
+            ></i>
             <i
               className="fas fa-trash-alt"
-              onClick={(e) => task.remove(e, task.id)}
+              onClick={(e) => task.action.remove(e, task.id)}
             ></i>
           </div>
         </li>
